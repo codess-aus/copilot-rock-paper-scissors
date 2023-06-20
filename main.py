83e60c1c968f3e9eca010ee341fdd3a2e62225e6
Michelle Mei-Ling Sandford <codesswa@outlook.com>
2023-06-20 16:24:07 +0800

    updates

---
+++ main.py
@@ -0,0 +1,28 @@
+# Write a rock, paper, scissors game
+# 1. Ask the user to input either rock, paper, or scissors
+# 2. Have the computer randomly pick one of the three options
+# 3. Compare the user's input to the computer's choice
+# 4. Print out the result
+# 5. Ask the user if they want to play again
+# 6. If they do, start over
+# 7. If they don't, exit the game
+# 8. Add a score counter
+# 9. Add a best of 3 or 5 option
+# 10. Add a way to quit the game at any time
+# 11. Add a way to see the score at any time
+# 12. Add a way to see the rules at any time
+# 13. Add a way to see the game history at any time
+# 14. Add a way to see the game instructions at any time
+# 15. Add a way to see the game credits at any time
+
+import random
+
+def main():
+    
+
+
+
+# Alt/
+# winget install sysinternals (for Zoomit)
+
+# shift control p format document
